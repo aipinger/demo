88e30a3972ff68d1a9dc9afdd300812e9f91601b
aipinger <285902467@qq.com>
2017-01-10 12:51:48 +0800

    "update"

--- src/main/java/com/umi/demo/gitdemo/App.java
+++ src/main/java/com/umi/demo/gitdemo/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello git1111122 " );
+        System.out.println( "Hello git1111122334 " );
     }
 }
